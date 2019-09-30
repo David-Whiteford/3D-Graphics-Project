dfc2244e6ee5e66d9198a113498b2b856d9a7e0a
David-Whiteford <c00204740@itcarlow.ie>
2019-09-30 13:00:33 +0100

    Lab2 Started enemy planets

---
+++ Lab2/Assets/EnemyPlanetController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPlanetController : MonoBehaviour
+{
+
+    public float enemySpeed;
+    private float dTime;
+    private Rigidbody2D rb;
+    private float moveHor;
+    private float moveVer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        dTime = 0;
+        RandomGen();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dTime += Time.deltaTime;
+        if (dTime >= 1.0f)
+        {
+            RandomGen();
+            dTime = 0.0f;  
+        }
+
+
+       
+    }
+    void RandomGen()
+    {
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
+        moveHor = Random.Range(-10.0f, 10.0f);
+        moveVer = Random.Range(-10.0f, 10.0f);
+
+        rb.AddForce(new Vector2(moveHor * enemySpeed, moveVer * enemySpeed));
+    }
+
+
+}
+
