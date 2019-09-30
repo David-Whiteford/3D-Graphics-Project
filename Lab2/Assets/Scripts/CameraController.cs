5d3e62e2c7385b34bcfaa367d8fb3946e8443b3d
johnfoley2048 <C00217207@itcarlow.ie>
2019-09-30 11:31:55 +0100

    Player controller, Camera controller, background border added

---
+++ Lab2/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = gameObject.transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        gameObject.transform.position = player.transform.position + offset;
+    }
+}
---
+++ Lab2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float startSpeed;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHor = Input.GetAxis("Horizontal");
+        float moveVer = Input.GetAxis("Vertical");
+
+        rb.AddForce(new Vector2(moveHor * startSpeed, moveVer * startSpeed));
+    }
+}
